1741b1be17db52d91b37a3ab19914e2cef6f1c91
Shang-Hung Tsai <seantsai726@gmail.com>
2018-04-09 13:08:26 -0400

    update script for load data

--- script/load_data.sh
+++ script/load_data.sh
@@ -4,26 +4,26 @@
 DATA_DIR="../data"
 if [ ! -d "$DATA_DIR" ]
 then
-    mkdir ../data
+    mkdir $DATA_DIR
 fi
 
 # Download data if not exist
-if [ ! -e ../data/311_Service_Requests.csv ]
+if [ ! -e $DATA_DIR/311_Service_Requests.csv ]
 then
     echo "Downloading 311_Service_Requests.csv" 
-    curl https://data.cityofnewyork.us/api/views/erm2-nwe9/rows.csv?accessType=DOWNLOAD --output ../data/311_Service_Requests.csv -s &
+    curl https://data.cityofnewyork.us/api/views/erm2-nwe9/rows.csv?accessType=DOWNLOAD --output $DATA_DIR/311_Service_Requests.csv -s &
 fi
 
-if [ ! -e ../data/DOB_Permit_Issuance.csv ]
+if [ ! -e $DATA_DIR/DOB_Permit_Issuance.csv ]
 then
     echo "Downloading DOB_Permit_Issuance.csv" 
-    curl https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD --output ../data/DOB_Permit_Issuance.csv -s &
+    curl https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD --output $DATA_DIR/DOB_Permit_Issuance.csv -s &
 fi
 
-if [ ! -e ../data/DOB_Job_Application_Filings.csv ]
+if [ ! -e $DATA_DIR/DOB_Job_Application_Filings.csv ]
 then
     echo "Downloading DOB_Job_Application_Filings" 
-    curl https://data.cityofnewyork.us/api/views/ic3t-wcy2/rows.csv?accessType=DOWNLOAD --output ../data/DOB_Job_Application_Filings.csv -s &
+    curl https://data.cityofnewyork.us/api/views/ic3t-wcy2/rows.csv?accessType=DOWNLOAD --output $DATA_DIR/DOB_Job_Application_Filings.csv -s &
 fi
 
 echo "Now the downloading job is running in the background"
