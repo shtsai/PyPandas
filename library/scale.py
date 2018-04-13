882ae436ad9a574974b1920e0f230fc1f41a27be
Chia-Hsien Lin <chl566@nyu.edu>
2018-04-13 01:33:44 -0400

    add the max_abs_function.

--- library/scale.py
+++ library/scale.py
@@ -2,6 +2,7 @@
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StandardScaler
 from pyspark.ml.feature import MinMaxScaler
+from pyspark.ml.feature import MaxAbsScaler
 from pyspark.sql.functions import udf
 from pyspark.sql.types import FloatType
 
@@ -82,4 +83,40 @@ def min_max_scale(dataFrame, inputColNames, Min=0.0, Max=1.0):
         return scaledDF
     else:
          raise ValueError("The inputColNames has to be string or string list.")
+
+def max_abs_scale(dataFrame, inputColNames):
+    if type(inputColNames) is str:
+        outputColName = "scaled " + inputColNames
+        assembler = VectorAssembler(inputCols=[inputColNames], \
+                outputCol="features")
+        assembledDF = assembler.transform(dataFrame)
+        scaler=MaxAbsScaler(inputCol="features", \
+                outputCol=outputColName)
+        scalerModel=scaler.fit(assembledDF)
+        scaledDF = scalerModel.transform(assembledDF).drop("features")
+        castVectorToFloat = udf(lambda v : float(v[0]), FloatType())
+        scaledDF = scaledDF.withColumn(outputColName, castVectorToFloat(outputColName)) 
+        print ("Successfully scale the column '{0:s}' to range (-1, 1) and create a new column '{1:s}'."\
+                .format(inputColNames, outputColName))
+        return scaledDF
+    elif type(inputColNames) is list:
+        scaledDF = dataFrame
+        for inputColName in inputColNames:
+            outputColName = "scaled " + inputColName
+            assembler = VectorAssembler(inputCols=[inputColName], \
+                    outputCol="features")
+            assembledDF = assembler.transform(scaledDF)
+            scaler=MaxAbsScaler(inputCol="features", \
+                    outputCol=outputColName)
+            scalerModel=scaler.fit(assembledDF)
+            scaledDF = scalerModel.transform(assembledDF).drop("features")
+            castVectorToFloat = udf(lambda v : float(v[0]), FloatType())
+            scaledDF = scaledDF.withColumn(outputColName, castVectorToFloat(outputColName)) 
+            print ("Successfully scale the column '{0:s}' to range (-1, 1) and create a new column '{1:s}'."\
+                    .format(inputColName, outputColName))
+
+        return scaledDF
+    else:
+         raise ValueError("The inputColNames has to be string or string list.")
+
 
