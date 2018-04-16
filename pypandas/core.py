28465656c0e5463c6ac994a47d0feec33428dc3d
plliao <pll273@nyu.edu>
2018-04-15 21:30:26 -0400

    Add apply udf function to handle single or multiple column input

--- pypandas/core.py
+++ pypandas/core.py
@@ -1,4 +1,12 @@
 
+def apply_udf(dataframe, columns, udf):
+    if type(columns) is list:
+        return apply_udf_to_columns(dataframe, columns, udf)
+    elif columns == "*":
+        return apply_udf_to_columns(dataframe, dataframe.columns, udf)
+    else:
+        return apply_udf_to_column(dataframe, columns, udf)
+
 def apply_udf_to_columns(dataframe, columns, udf):
     expression_cols = []
     for col in dataframe.columns:
