36d1630c40fe52439dccc5fa53baa96fc1a86f31
Chia-Hsien Lin <chl566@nyu.edu>
2018-05-02 19:34:41 -0400

    add optimus_experiment.py

---
+++ experiment/textcleaner_experiment/optimus_experiment.py
@@ -0,0 +1,10 @@
+import sys, time
+import optimus as op
+import pypandas.datasets import *
+
+starttime = time.time()
+df = load_data_311("aws")
+transformer = op.DataFrameTransformer(df)
+transformer.remove_special_chars(columns='*')
+transformer.select(["Unique Key", "Latitude", "Longitude"]).show(20, False)
+print("The optimus takes: " + str(time.time() - starttime) + " sec to clean the columns.")
