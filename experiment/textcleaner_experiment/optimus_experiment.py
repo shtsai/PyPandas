731dbb26e9450fdb68e602fe717072b299163b46
Chia-Hsien Lin <chl566@nyu.edu>
2018-05-02 20:04:37 -0400

    add the comparison of the textcleaner and the optimus experiment

--- experiment/textcleaner_experiment/optimus_experiment.py
+++ experiment/textcleaner_experiment/optimus_experiment.py
@@ -2,9 +2,31 @@ import sys, time
 import optimus as op
 import pypandas.datasets import *
 
-starttime = time.time()
-df = load_data_311("aws")
-transformer = op.DataFrameTransformer(df)
-transformer.remove_special_chars(columns='*')
-transformer.select.show(20, False)
-print("The optimus takes: " + str(time.time() - starttime) + " sec to clean the columns.")
+def clean(df):
+    starttime = time.time()
+    transformer = op.DataFrameTransformer(df)
+    transformer.remove_special_chars(columns='*')
+    transformer.show(20, False)
+    print("The optimus takes: " + str(time.time() - starttime) + " sec to clean the columns.")
+
+def load_data():
+    data = sys.argv[1]
+    if data == "job":
+        df = load_data_job("aws")
+        print("[data_job]: ")
+    elif data == "311":
+        df = load_data_311("aws")
+        print("[data_311]: ")
+    elif data == "permit":
+        df = load_data_permit("aws")
+        print("[data_permit]: ")
+    else:
+        raise ValueError("Invalid argument.")
+    return df
+
+def main():
+    df = load_data()    
+    clean(df)
+
+if __name__ == "__main__":
+    main()
