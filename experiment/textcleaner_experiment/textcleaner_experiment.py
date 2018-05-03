92d639bf82f8c203cf160e9c3e2a680201b446ec
Chia-Hsien Lin <chl566@nyu.edu>
2018-05-02 22:53:06 -0400

    textcleaner_experiment.py

--- experiment/textcleaner_experiment/textcleaner_experiment.py
+++ experiment/textcleaner_experiment/textcleaner_experiment.py
@@ -1,30 +1,29 @@
-import sys, time
-from pypandas.textCleaner import clean_text, sub_with_pattern
+import sys, time 
+from pypandas.datasets 
+import * from pypandas.textCleaner 
+import clean_text, sub_with_pattern 
 
-def clean(df):
-    starttime = time.time()
-    df = sub_with_pattern(df, '*', '!"#$%&/()=?', '')
-    df.show(20, False)
-    print("Our textcleaner takes: " + str(time.time() - starttime) + " sec to clean the columns.")
+def clean(df): 
+    df = sub_with_pattern(df, '*', '!"#$%&/()=?', '') 
+    df.show(20, False) 
 
-def load_data():
-    data = sys.argv[1]
-    if data == "job":
-        df = load_data_job("aws")
-        print("[data_job]: ")
-    elif data == "311":
-        df = load_data_311("aws")
-        print("[data_311]: ")
-    elif data == "permit":
-        df = load_data_permit("aws")
-        print("[data_permit]: ")
-    else:
-        raise ValueError("Invalid argument.")
-    return df
+def load_data(): 
+    data = sys.argv[1] 
+    if data == "job": 
+        df = load_data_job("aws") 
+    elif data == "311": 
+        df = load_data_311("aws") 
+    elif data == "permit": 
+        df = load_data_permit("aws") 
+    else: 
+        raise ValueError("Invalid argument.") 
+    return df 
 
-def main():
-    df = load_data()    
-    clean(df)
-
-if __name__ == "__main__":
+def main(): 
+    starttime = time.time() 
+    df = load_data() 
+    clean(df) 
+    print("Our textcleaner takes: " + str(time.time() - starttime) + " sec to clean the data " + sys.argv[1]) 
+    
+if __name__ == "__main__": 
     main()
