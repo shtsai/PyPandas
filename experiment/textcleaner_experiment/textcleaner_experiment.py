9dd28bd6fd131146d185912816cfe8c59b40bd92
plliao <pll273@nyu.edu>
2018-05-14 00:37:40 +0000

    Fix textcleaner and optimus experiment bug

--- experiment/textcleaner_experiment/textcleaner_experiment.py
+++ experiment/textcleaner_experiment/textcleaner_experiment.py
@@ -2,25 +2,27 @@ import sys, time
 from pypandas.datasets import *
 from pypandas.textCleaner import * 
 
-def clean(df): 
-    df = sub_with_pattern(df, '*', '!"#$%&/()=?', '') 
+
+def clean(df):
+    valid_cols = [c for (c, t) in filter(lambda t: t[1] == 'string', df.dtypes)]
+    df = sub_with_pattern(df, valid_cols, '!\"#\$%&/\(\)=\?', '')
     df.count()
 
 def load_data(): 
-    data = sys.argv[1] 
-    if data == "job": 
-        df = load_data_job("aws") 
-    elif data == "311": 
-        df = load_data_311("aws") 
+    data = sys.argv[1]
+    if data == "job":
+        df = load_data_job("aws")
+    elif data == "311":
+        df = load_data_311("aws")
     elif data == "permit": 
-        df = load_data_permit("aws") 
+        df = load_data_permit("aws")
     else: 
         raise ValueError("Invalid argument.") 
     return df 
 
 def main(): 
-    starttime = time.time() 
-    df = load_data() 
+    df = load_data()
+    starttime = time.time()
     clean(df) 
     print("Our textcleaner takes: " + str(time.time() - starttime) + " sec to clean the data " + sys.argv[1]) 
     
